4daf27c0f38d891ce00c68ccd8b0582f77bd6801
YAS <YAS@DESKTOP-PDVS5LP>
2021-01-24 22:53:57 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        ArrayList customers = new ArrayList();
+        public void Delete(Customer customer)
+        {
+            customers.Remove(customer);
+            Console.WriteLine("Customer Id Number " + customer.Id + " has been deleted.");
+        }
+        public void Add(Customer customer)
+        {
+            customers.Add(customer);
+        }
+        public void List()
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Id: " + customer.Id + " ,Name: " + customer.Name + " , Lastname: " + customer.Lastame+" , City: "+customer.City);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer()
+            {
+                Id = 1,
+                Name = "Yasemen",
+                Lastame = "Küçükoğlu",
+                City = "Istanbul"
+            };
+            Customer customer2 = new Customer()
+            {
+                Id = 2,
+                Name = "İbrahim",
+                Lastame = "Delbe",
+                City = "Ankara"
+            };
+            Customer customer3 = new Customer()
+            {
+                Id = 3,
+                Name = "Ali",
+                Lastame = "Veli",
+                City = "Bursa"
+            };
+
+      
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+
+            customerManager.List();
+
+            customerManager.Delete(customer2);
+            customerManager.List();
+
+
+
+        }
+    }
+}
